2deeb8b344738b104cc9033f117bb5bbfcec86ec
Edwin Felipe <edwinisfelipe25@gmail.com>
2020-11-30 13:09:56 -0400

    Add createdAt to user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,21 +1,22 @@
 const { Schema, model } = require("mongoose");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const UserSchema = new Schema({
   name: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   userName: { type: String },
+  createdAt: { type: Date, default: Date.now },
 });
 
-UserSchema.pre('save', async function(){
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(this.password, salt);
-    this.password = hash;
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(this.password, salt);
+  this.password = hash;
 });
 
-UserSchema.methods.comparePassword = async function (password){
-    return await bcrypt.compare(password, this.password);
-}
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-module.exports = model('User', UserSchema);
+module.exports = model("User", UserSchema);
