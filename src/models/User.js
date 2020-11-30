0fa06b1b2993ac6e4ee35e5ce2effe6298b59045
Edwin Felipe <edwinisfelipe25@gmail.com>
2020-11-30 13:07:27 -0400

    Create user model

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require('bcryptjs');
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  userName: { type: String },
+});
+
+UserSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+});
+
+UserSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password, this.password);
+}
+
+module.exports = model('User', UserSchema);
